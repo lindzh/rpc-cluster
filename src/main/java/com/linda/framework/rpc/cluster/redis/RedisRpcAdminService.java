f11d83709b39eee906c13e2eacaf5120f5923dae
linda <linsony0@163.com>
2015-05-19 19:20:29 +0800

    misc:add redis rpc cluster admin

--- src/main/java/com/linda/framework/rpc/cluster/redis/RedisRpcAdminService.java
+++ src/main/java/com/linda/framework/rpc/cluster/redis/RedisRpcAdminService.java
@@ -1,22 +1,80 @@
 package com.linda.framework.rpc.cluster.redis;
 
 import java.util.List;
+import java.util.Set;
+
+import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
 
 import com.linda.framework.rpc.RpcService;
+import com.linda.framework.rpc.Service;
 import com.linda.framework.rpc.cluster.RpcHostAndPort;
 import com.linda.framework.rpc.cluster.admin.RpcAdminService;
 
-public class RedisRpcAdminService implements RpcAdminService{
+public class RedisRpcAdminService implements RpcAdminService,Service{
 	
-	private RedisRpcClient redisRpcClient;
+	private RedisRpcClient redisRpcClient = new RedisRpcClient();
+
+	public String getRedisHost() {
+		return redisRpcClient.getRedisHost();
+	}
+
+	public void setRedisHost(String host) {
+		redisRpcClient.setRedisHost(host);
+	}
+
+	public int getRedisPort() {
+		return redisRpcClient.getRedisPort();
+	}
+
+	public void setRedisPort(int port) {
+		redisRpcClient.setRedisPort(port);
+	}
+
+	public Set<String> getRedisSentinels() {
+		return redisRpcClient.getRedisSentinels();
+	}
+
+	public void setRedisSentinels(Set<String> sentinels) {
+		redisRpcClient.setRedisSentinels(sentinels);
+	}
+
+	public String getRedisMasterName() {
+		return redisRpcClient.getRedisMasterName();
+	}
+
+	public void setRedisMasterName(String masterName) {
+		redisRpcClient.setRedisMasterName(masterName);
+	}
+
+	public GenericObjectPoolConfig getRedisPoolConfig() {
+		return redisRpcClient.getRedisPoolConfig();
+	}
+
+	public void setRedisPoolConfig(GenericObjectPoolConfig poolConfig) {
+		redisRpcClient.setRedisPoolConfig(poolConfig);
+	}
+	
+	private RedisRpcClientExecutor getRedisExecutor(){
+		return (RedisRpcClientExecutor)redisRpcClient.getRemoteExecutor();
+	}
 
 	@Override
 	public List<RpcHostAndPort> getRpcServers() {
-		return null;
+			return getRedisExecutor().getHostAndPorts();
 	}
 
 	@Override
 	public List<RpcService> getRpcServices(RpcHostAndPort rpcServer) {
-		return null;
+		return getRedisExecutor().getServerService(rpcServer);
+	}
+
+	@Override
+	public void startService() {
+		redisRpcClient.startService();
+	}
+
+	@Override
+	public void stopService() {
+		redisRpcClient.startService();		
 	}
 }
